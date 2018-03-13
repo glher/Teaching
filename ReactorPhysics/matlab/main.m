2d2aedbf8806234746f20b3f01d63e6abb54fb5d
Guillaume Lher <glher@mymail.mines.edu>
2018-03-13 12:26:51 -0600

    Comments

--- ReactorPhysics/matlab/main.m
+++ ReactorPhysics/matlab/main.m
@@ -1,7 +1,14 @@
+% Adds the path necessary for Matlab to find the
+% functions you are going to call.
 addpath('src')
 addpath('cst')
+% Loads the global parameters of the problem
 parameters();
+% Reads the geometry and obtain the diffusion
+% coefficients at every point of the solver grid.
 diffusion_coefficient = geometry();
+% Solves the system of equation
 flux = solver1(diffusion_coefficient);
+% Graphs the solution of the flux.
 graphs(flux)
 
